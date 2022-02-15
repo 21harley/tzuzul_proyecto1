b513f5f85e62b5001a4fb0c202f4c7a3fe5350ce
21harley <jhonharleyllanesescobar@hotmail.com>
2022-02-15 06:12:13 -0400

    add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {Link} from 'react-router-dom';
 import "./../css/nav.css";
 
 const SEARCH_API =
@@ -45,6 +46,8 @@ function Navbar() {
 
   return (
     <nav className={navbar ? "nav active" : "nav"}>
+      
+
       <Link to={"/"}>
       <div className="logo"></div>
       </Link>
